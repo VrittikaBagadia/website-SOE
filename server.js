d998a3618f38c689648ffe2ee5edbbf412b1b45a
Emily Pazienza <emp88@drexel.edu>
2018-12-11 10:56:38 +0100

    Changed server setup

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 var express = require('express');
-var app = express();
-var http = require('http');
 var fs = require('fs');
 
-var index = fs.readFileSync('index.html');
-var port = 3000;
+// Set up the express app
+var app = express();
 app.use(express.static('public'))
 
-http.createServer(function (req, res) {
-  if (req.url === '/') { // asking for the html page
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(index);
-  } else { // asking for a static resource file
-    fs.createReadStream('public' + req.url).pipe(res);
-  }
-}).listen(port);
+var index = fs.readFileSync('index.html');
+
+app.get('/', (err, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end(index);
+});
+
+var port = 3000;
+
+app.listen(port, () => {
+  console.log(`server running on port ` + port);
+});
