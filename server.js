034b447f05e46f3d4e6bc32a10bbae42d1fec785
Tony Sax <tony@wantenna.com>
2018-12-11 11:54:19 +0100

    Added api request based on boolean json outcome.

--- server.js
+++ server.js
@@ -10,17 +10,31 @@ app.get('/', (err, res) => {
 	res.end(index);
 });
 
-// app.get('/working', (err, res) => {
-//   res.status(200);
-//   res.json({ working: true });
-//   res.end();
-// });
+app.get('/working', (err, res) => {
+  res.status(200);
+  res.json({ working: true });
+  res.end();
+});
 
-// app.get('/amazon', (err, res) => {
-//   res.status(200);
-//   res.send('https://www.amazon.com/');
-//   res.end();
-// });
+app.get('/amazon', (err, res) => {
+  res.status(200);
+  res.send('https://www.amazon.com/');
+  res.end();
+});
+
+app.get('/description', function(req,res) {
+    // console.log(req.query.json);
+    // res.send('Response send to client::'+req.query.json);
+    if (req.query.json === "true") {
+      res.json({ description: true });
+      res.end();
+    }
+    if (req.query.json === "false") {
+      res.send("localhost:3000/description");
+      res.end();
+    }
+
+});
 
 var port = 3000;
 
