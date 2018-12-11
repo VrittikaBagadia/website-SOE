111f937cec1c8a5fc43f654dc94f8a38040b8c04
Emily Pazienza <emp88@drexel.edu>
2018-12-11 11:21:12 +0100

    Updates to the server

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var fs = require('fs');
-
-// Set up the express app
 var app = express();
 app.use(express.static('public'))
 
@@ -11,6 +9,18 @@ app.get('/', (err, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
 	res.end(index);
 });
+
+// app.get('/working', (err, res) => {
+//   res.status(200);
+//   res.json({ working: true });
+//   res.end();
+// });
+
+// app.get('/amazon', (err, res) => {
+//   res.status(200);
+//   res.send('https://www.amazon.com/');
+//   res.end();
+// });
 
 var port = 3000;
 
