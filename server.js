c26206a47ab3e65450827d3e3cf426d3ebc4099a
Emily Pazienza <emp88@drexel.edu>
2018-12-18 16:20:28 +0100

    Edited description endpoint for error handling

--- server.js
+++ server.js
@@ -28,18 +28,27 @@ app.get('/amazon', (err, res) => {
   res.end();
 });
 
-app.get('/description', function(req,res) {
-    // console.log(req.query.json);
-    // res.send('Response send to client::'+req.query.json);
-    if (req.query.json === "true") {
-      res.json({ description: true });
-      res.end();
-    }
-    if (req.query.json === "false") {
-      res.send("localhost:3000/description");
-      res.end();
-    }
+var des = "- Use of solar energy\n- Adapted to your biological cycle\n- Brighter in the morning " +
+          "and it decreases with the time\n- Differents lights colours\n- Long lasting\n- LED bulbs" +
+          " have a life expectancy of operational work up to 100, 000 hours or eleven years of "+
+          "non-stop operation.";
 
+app.get('/description', (req,res) => {
+    if (req.query.json === "true") {
+      res.status(200);
+      res.json({ description: des });
+      res.end();
+    } else if (req.query.json === "false") {
+      res.status(200);
+      res.send(des);
+      res.end();
+    } else {
+      res.status(400);
+      res.json({error: {
+        code: 400,
+        description: "Invalid parameters"}});
+      res.end();
+    }
 });
 
 var port = 3000;
